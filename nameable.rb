9620d0b3a28ed11cce5fa62b26f0864ec7c4cc80
Andy Menutti <andymenutti@gmail.com>
2022-11-01 17:08:02 -0300

    add decorator_base

---
+++ nameable.rb
@@ -0,0 +1,5 @@
+class Nameable < person
+  def correct_name
+    raise NotImplementedError
+  end
+end
--- person.rb
+++ person.rb
@@ -1,9 +1,19 @@
-class Person
+require_relative 'nameable'
+# Make sure that this class has a method correct_name implemented. It should simply return the name attribute.
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, parent_permission, name = 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
