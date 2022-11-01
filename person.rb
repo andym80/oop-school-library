72f79437706b36992fd974ee6c6266c84bb33a69
Andy Menutti <andymenutti@gmail.com>
2022-11-01 17:38:51 -0300

    fix person.rb require_relative

--- person.rb
+++ person.rb
@@ -1,11 +1,14 @@
-require_relative 'nameable'
-# Make sure that this class has a method correct_name implemented. It should simply return the name attribute.
+require_relative './nameable'
+require_relative './decorator_capitalize'
+require_relative './decorator_trimmer'
+require_relative './decorator_base'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, parent_permission, name = 'Unknown')
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
