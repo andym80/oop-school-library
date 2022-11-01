603212025279c6c0f68cd23dfa1ec9323531edf5
Andy Menutti <andymenutti@gmail.com>
2022-10-31 22:07:16 -0300

    fix teacher.rb & student.rb

--- teacher.rb
+++ teacher.rb
@@ -3,12 +3,12 @@ require_relative './person'
 class Teacher < Person
   attr_accessor :specialization, :id
 
-  def initialize(specialization, age, parent_permission, name)
+  def initialize(specialization, age, parent_permission, _name)
     @specialization = specialization
     super(
       age,
-    name,
-    parent_permission
+    parent_permission,
+    name
   )
   end
 end
