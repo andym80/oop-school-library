67770319b8ff7b16b7d2c8ff04be4180735c2213
Andy Menutti <andymenutti@gmail.com>
2022-11-01 22:31:02 -0300

    fix person.rb private:public

--- person.rb
+++ person.rb
@@ -1,13 +1,12 @@
 require_relative './nameable'
 require_relative './decorator_capitalize'
 require_relative './decorator_trimmer'
-require_relative './decorator_base'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, parent_permission, name = 'Unknown')
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
@@ -19,27 +18,20 @@ class Person < Nameable
     @name
   end
 
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
 end
 
 person = Person.new(22, 'maximilianus')
-
 person.correct_name
-
 capitalized_person = CapitalizeDecorator.new(person)
-
 puts capitalized_person.correct_name
-
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-
 puts capitalized_trimmed_person.correct_name
