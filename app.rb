2b77c44cbc3336d5f963f6e570da22da129a6581
Andy Menutti <andymenutti@gmail.com>
2022-11-03 18:03:59 -0300

    fix app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
-require_relative './book.rb'
-require_relative './rental.rb'
-require_relative './person.rb'
-require_relative './student.rb'
-require_relative './teacher.rb'
-require_relative './decorator_capitalize.rb'
-require_relative './decorator_trimmer.rb'
+require_relative './book'
+require_relative './rental'
+require_relative './student'
+require_relative './teacher'
+require_relative './person'
 
 class App
   attr_reader :books, :people, :rentals
@@ -15,98 +13,85 @@ class App
     @rentals = SaveFiles.load_rentals
   end
 
-  def run
-    p 'Welcome to School Library App!'
-    loop do
-	  p 'Please choose an option by entering a number:'
-	  p '1 - List all books'
-	  p '2 - List all people'
-	  p '3 - Create a person'
-	  p '4 - Create a book'
-	  p '5 - Create a rental'
-	  p '6 - List all rentals for a given person id'
-	  p '7 - Exit'
-	  option = gets.chomp
-	  case option
-	  when '1'
-		list_books
-	  when '2'
-		list_people
-	  when '3'
-		create_person
-	  when '4'
-		create_book
-	  when '5'
-		create_rental
-	  when '6'
-		list_rentals
-	  when '7'
-		p 'Thank you for using this app!'
-		break
-	  else
-		p 'That is not a valid option'
-	  end
-	end
+  def list_books
+    @books.each do |book|
+      p "Title: #{book.title}, Author: #{book.author}"
+    end
   end
 
-      # List all books
+  def create_book
+    p 'Title:'
+    title = gets.chomp
+    p 'Author:'
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books.push(book)
+    p 'Book created successfully'
+  end
 
-      get '/books' do
-        @books = Book.all
-        erb :books
-      end
+  def list_people
+    @people.each do |person|
+      p "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
 
-      # List all people
+  def create_person
+    p 'Do you want to create a student (1) or a teacher (2)? [Input the number]'
+    person_type = gets.chomp
+    case person_type
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      p 'Invalid option'
+    end
+  end
 
-      get '/people' do
-        @people = Person.all
-        erb :people
-        if @people.empty?
-          p 'There are no people created'
-          @people.each do |person|
-            p "Type: #{person.class}, Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-          end
-        else
-          p 'There are no books created'
-          @books.each do |book|
-            p "Title: #{book.title}, Author: #{book.author}"
-		  end
+  def create_student
+    p 'Age:'
+    age = gets.chomp
+    p 'Name:'
+    name = gets.chomp
+    p 'Has parent permission? [Y/N]'
+    parent_permission = gets.chomp
+    parent_permission = parent_permission.casecmp('y').zero?
+    student = Student.new(age, name, parent_permission)
+    @people.push(student)
+    p 'Person created successfully'
+  end
 
-# Create a person (POST /people)
+  def create_teacher
+    p 'Age:'
+    age = gets.chomp
+    p 'Name:'
+    name = gets.chomp
+    p 'Specialization:'
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @people.push(teacher)
+    p 'Person created successfully'
+  end
 
-post '/people' do
-  @person = Person.new(params[:name], params[:age], params[:parent_permission])
-  p 'method called'
-  is_permitted = params[:parent_permission].casecmp('y').zero?
-  @person = if is_permitted
-              Student.new(params[:name], params[:age], params[:parent_permission])
-            else
-              Teacher.new(params[:name], params[:age], params[:parent_permission])
-			  			end
-  @person.save
-  redirect '/people'
-  @people = person
-end
+  def create_rental
+    p 'ID of the person:'
+    person_id = gets.chomp
+    p 'ID of the book:'
+    book_id = gets.chomp
+    p 'Date:'
+    date = gets.chomp
+    rental = Rental.new(date, book_id, person_id)
+    @rentals.push(rental)
+    p 'Rental created successfully'
+  end
 
-# Create a new book (POST /books)
-
-post '/books' do
-  @book = Book.new(params[:title], params[:author])
-  @book.save
-  redirect '/books'
-end
-
-# Create a new rental (POST /rentals)
-
-post '/rentals' do
-  @rental = Rental.new(params[:date], params[:book], params[:person])
-  @rental.save
-  redirect '/rentals'
-end
-
-# List all rentals for a given person id
-
-get '/rentals/:id' do
-  @rentals = Rental.find_by_person_id(params[:id])
-  erb :rentals
+  def list_rentals
+    p 'ID of the person:'
+    id = gets.chomp
+    rentals = @rentals.filter { |rental| rental.person.id == id }
+    puts 'Rentals:'
+    rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
+    end
+  end
 end
