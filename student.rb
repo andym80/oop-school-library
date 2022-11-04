b0029280f60a3eeb045513c6d0c94ffc69f80a6a
Andy Menutti <andymenutti@gmail.com>
2022-11-04 17:05:54 -0300

    fix student.rb

--- student.rb
+++ student.rb
@@ -11,8 +11,4 @@ class Student < Person
   def classroom=(classroom)
     classroom.students.push(self) unless classroom.students.include?(self)
   end
-
-  def play_hooky
-    "¯\(ツ)/¯"
-  end
 end
